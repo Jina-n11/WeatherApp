0c17b3c0f2b36e173c44cd3da9db2203ce9a9069
karrarjasim <k.jasim@s-gate.iq>
2022-08-10 16:27:24 +0300

    change observable type

--- app/src/main/java/com/cheese/weatherapp/data/repository/WeatherRepository.kt
+++ app/src/main/java/com/cheese/weatherapp/data/repository/WeatherRepository.kt
@@ -3,14 +3,15 @@ package com.cheese.weatherapp.data.repository
 import com.cheese.weatherapp.data.State
 import com.cheese.weatherapp.data.models.Weather
 import com.cheese.weatherapp.data.services.BaseWeatherService
+import com.example.mikmok.data.models.WeatherMain
 import io.reactivex.rxjava3.core.Observable
 
 interface WeatherRepository {
-    fun getWeatherByCityName(cityName: String): Observable<State<Weather>>
+    fun getWeatherByCityName(cityName: String): Observable<State<WeatherMain>>
 }
 
 class WeatherRepositoryImp(private val weatherService: BaseWeatherService) : WeatherRepository {
-    override fun getWeatherByCityName(cityName: String): Observable<State<Weather>> {
+    override fun getWeatherByCityName(cityName: String): Observable<State<WeatherMain>> {
         return Observable.create { emit ->
             emit.onNext(State.Loading)
             emit.onNext(weatherService.getWeatherByCityName(cityName))
--- app/src/main/java/com/cheese/weatherapp/data/services/WeatherService.kt
+++ app/src/main/java/com/cheese/weatherapp/data/services/WeatherService.kt
@@ -2,20 +2,21 @@ package com.cheese.weatherapp.data.services
 
 import com.cheese.weatherapp.data.State
 import com.cheese.weatherapp.data.models.Weather
+import com.example.mikmok.data.models.WeatherMain
 import com.google.gson.Gson
 import okhttp3.OkHttpClient
 
 interface BaseWeatherService {
-    fun getWeatherByCityName(cityName: String): State<Weather>
+    fun getWeatherByCityName(cityName: String): State<WeatherMain>
 
 }
 
 class WeatherService : BaseWeatherService {
     private val client = OkHttpClient()
-    override fun getWeatherByCityName(cityName: String): State<Weather> {
+    override fun getWeatherByCityName(cityName: String): State<WeatherMain> {
         val response = client.newCall(RequestHelper.makeWeatherRequest(cityName)).execute()
         return if (response.isSuccessful) {
-            Gson().fromJson(response.body?.string(), Weather::class.java).run {
+            Gson().fromJson(response.body?.string(), WeatherMain::class.java).run {
                 State.Success(this)
             }
         } else {
