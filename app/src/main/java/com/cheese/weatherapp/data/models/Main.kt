0d9f6e514b2f0002f9dfc509ab165efa0ee25c4c
Falah Hassan <devfalahhassan@gmail.com>
2022-08-11 16:34:31 -0700

    add serialized name to all models

--- app/src/main/java/com/cheese/weatherapp/data/models/Main.kt
+++ app/src/main/java/com/cheese/weatherapp/data/models/Main.kt
@@ -3,14 +3,14 @@ package com.cheese.weatherapp.data.models
 import com.google.gson.annotations.SerializedName
 
 data class Main (
+    @SerializedName("temp")
     val temp: Double,
-
     @SerializedName("temp_min")
     val tempMin: Double,
-
     @SerializedName("temp_max")
     val tempMax: Double,
-
+    @SerializedName("pressure")
     val pressure: Double,
+    @SerializedName("humidity")
     val humidity: Double
 )
--- app/src/main/java/com/cheese/weatherapp/data/models/Wind.kt
+++ app/src/main/java/com/cheese/weatherapp/data/models/Wind.kt
@@ -1,5 +1,8 @@
 package com.cheese.weatherapp.data.models
 
+import com.google.gson.annotations.SerializedName
+
 data class Wind (
+    @SerializedName("speed")
     val speed: Double,
 )
